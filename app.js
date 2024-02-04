29149033a881bbd04a2a8d16d7d829ce763082bd
Vinsyahputra <vinthecyborg@gmail.com>
2024-02-04 19:35:44 +0700

    add some basic route & middleware with express JS

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
+
+// app.use((req, res, next)=>{
+//     console.log('ini seperti __construct di php');
+// next();
+// })
+
+// app.use digunakan untuk semua method di http (get,post,patch,del,...)
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+// controller /admin
+app.use('/admin',adminRoute);
+app.use(shopRoute);
+
+//return 404 status
+app.use('/', (req, res, next) => {
+    res.status(404).send('<h1>Page Not Found!.</h1>');
+});
+
+// server.listen(3000);
+app.listen(3000, () => {
+    console.log(`Server started on port 3000`);
+});
